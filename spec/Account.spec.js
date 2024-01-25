a22b93236a745987e6c64c51eabf2200bb59875f
Lok Sze Chung <lschung@hotmail.co.uk>
2024-01-25 00:04:13 +0000

    Add test for getBalance

--- spec/Account.spec.js
+++ spec/Account.spec.js
@@ -1,14 +1,31 @@
 import Account from "../src/Account.js"
 
-describe("Accounts tests", ()=>{
+describe("Accounts tests", () => {
 
-  describe("Create account tests", ()=>{
+  describe("Create account tests", () => {
     it("Should create a new instance of the account class", () => {
       
       const testAccount = new Account();
 
       expect(testAccount).toBeInstanceOf(Account);
-  });
+    });
+
+    it("getBalance() should return 0 when balance is 0", () => {
+      
+      const testAccount = new Account();
+
+      expect(testAccount.getBalance()).toEqual(0);
+    });
+  })
+
+  describe("Deposit funds tests", () => {
+    it("Depositing 100 into Account should increase balance by 100", () => {
+      
+      const testAccount = new Account();
+      testAccount.deposit(100)
+
+      expect(testAccount.getBalance()).toEqual(100);
+    });
   })
 
 })
--- src/Account.js
+++ src/Account.js
@@ -1,9 +1,15 @@
 class Account {
   static #nextId = 1234;
   #id
+  #balance
 
   constructor(){
     this.#id = Account.#nextId++
+    this.#balance = 0
+  }
+
+  getBalance() {
+    return this.#balance;
   }
 }
 
